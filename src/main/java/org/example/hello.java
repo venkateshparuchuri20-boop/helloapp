410fc82173281af9299290e48f75dbc5c6e63336
Venkatesh <venkateshparuchuri20@gmail.com>
2026-04-12 23:32:10 +0530

    uc4 done

--- src/main/java/org/example/hello.java
+++ src/main/java/org/example/hello.java
@@ -1,9 +1,20 @@
-public class hello{
-    public static void main(String[] args){
-        String name="venkatesh";
-        if(args.length>0){
-            name=args[0];
+public class hello {
+    public static void main(String[] args) {
+
+        String message = "";
+
+        if (args.length == 0) {
+            message = "World";
+        } else {
+            for (int i = 0; i < args.length; i++) {
+                message += args[i];
+
+                if (i < args.length - 1) {
+                    message += ", ";
+                }
+            }
         }
-        System.out.print("Hello,"+name+"!");
+
+        System.out.println("Hello, " + message + "!");
     }
 }
