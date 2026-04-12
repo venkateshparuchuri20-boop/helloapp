900af72c5ad17b77d76330c7c213713b6b4de2d6
Venkatesh <venkateshparuchuri20@gmail.com>
2026-04-12 23:15:45 +0530

    uc5 done

--- src/main/java/org/example/hello.java
+++ src/main/java/org/example/hello.java
@@ -1,6 +1,20 @@
-public class hello{
-    public static void main(String[] args){
+public class hello {
+    public static void main(String[] args) {
 
-        System.out.print("Hello World");
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } else {
+
+            StringBuilder names = new StringBuilder();
+
+            for (String name : args) {
+                if (names.length() > 0) {
+                    names.append(", ");
+                }
+                names.append(name);
+            }
+
+            System.out.println("Hello, " + names + "!");
+        }
     }
 }
