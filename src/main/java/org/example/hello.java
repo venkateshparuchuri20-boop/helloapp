9cb69b29af0f3da4b649f5d0a76abe3bc2387756
Venkatesh <venkateshparuchuri20@gmail.com>
2026-04-12 23:40:50 +0530

    uc7 done

--- src/main/java/org/example/hello.java
+++ src/main/java/org/example/hello.java
@@ -1,17 +1,12 @@
-public class hello {
+public class HelloApp {
     public static void main(String[] args) {
 
+        // Case 1: No arguments
         if (args.length == 0) {
             System.out.println("Hello, World!");
         } else {
-
-            StringBuilder nameBuilder = new StringBuilder();
-
-            for (String name : args) {
-                nameBuilder.append(name).append(", ");
-            }
-
-            String names = nameBuilder.substring(0, nameBuilder.length() - 2);
+            // Case 2: Use String.join()
+            String names = String.join(", ", args);
 
             System.out.println("Hello, " + names + "!");
         }
