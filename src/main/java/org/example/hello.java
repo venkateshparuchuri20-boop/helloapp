88ac2bea0c4afcb9cff0495ac598237c7f142e47
Venkatesh <venkateshparuchuri20@gmail.com>
2026-04-12 23:34:38 +0530

    uc6 done

--- src/main/java/org/example/hello.java
+++ src/main/java/org/example/hello.java
@@ -1,20 +1,19 @@
 public class hello {
     public static void main(String[] args) {
 
-        String message = "";
-
         if (args.length == 0) {
-            message = "World";
+            System.out.println("Hello, World!");
         } else {
-            for (int i = 0; i < args.length; i++) {
-                message += args[i];
 
-                if (i < args.length - 1) {
-                    message += ", ";
-                }
+            StringBuilder nameBuilder = new StringBuilder();
+
+            for (String name : args) {
+                nameBuilder.append(name).append(", ");
             }
-        }
 
-        System.out.println("Hello, " + message + "!");
+            String names = nameBuilder.substring(0, nameBuilder.length() - 2);
+
+            System.out.println("Hello, " + names + "!");
+        }
     }
 }
